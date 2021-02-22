5dd75894d4d04dda1c79ad05b4e5681344a17330
manoj <manojhegde73@gmail.com>
2021-02-22 18:58:43 +0530

    Updates

---
+++ precache-manifest.0af7a9a024eeea46ed75bf6172b5e864.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a69ef3426f9bbd6e5b38caaf9063235",
+    "url": "/shoppingcart-react/index.html"
+  },
+  {
+    "revision": "15d3313a5db62ca8bc36",
+    "url": "/shoppingcart-react/static/css/main.808a8821.chunk.css"
+  },
+  {
+    "revision": "19b1acd7ad304b1c5ada",
+    "url": "/shoppingcart-react/static/js/2.d7ec0aa2.chunk.js"
+  },
+  {
+    "revision": "15d3313a5db62ca8bc36",
+    "url": "/shoppingcart-react/static/js/main.6b4ded69.chunk.js"
+  },
+  {
+    "revision": "cbbd4baa08e53fff0ae3",
+    "url": "/shoppingcart-react/static/js/runtime-main.8aa7a781.js"
+  },
+  {
+    "revision": "a45e62daad05328e13d71ae9c0a5e15b",
+    "url": "/shoppingcart-react/static/media/spinner.a45e62da.gif"
+  }
+]);
